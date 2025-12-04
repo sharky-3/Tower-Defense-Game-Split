ed303f15013830d84aed4a46fb17ccc4c9b9547a
sharky-3 <whitebunnypue@gmail.com>
2025-12-04 11:24:52 +0100

    Tower take damage

--- Scripts/assets/player_tower.gd
+++ Scripts/assets/player_tower.gd
@@ -1,9 +1,9 @@
 extends Node3D
 
+# --- Constants / Exported Data ---
 @export_category("Main")
-@export var health: float
+@export var tower_healt: float
 @export var max_health: float
-@export var is_alive: bool = true
 
 @export_category("Visual/Audio")
 @export var damage_effect: bool
@@ -11,8 +11,24 @@ extends Node3D
 @export var hit_sound: AudioStream
 @export var death_sound: AudioStream
 
-func take_attack_damage(amount: int) -> void:
-	health -= amount
-	if health <= 0:
-		is_alive = false
-		#queue_free() 
+# --- Node references ---
+@onready var player_tower: Node3D = $"."
+
+# --------------------------------------------------------------------
+# Take Damage
+# --------------------------------------------------------------------
+
+func take_attack_damage(amount: float) -> void:
+	print("damage", amount)
+	_attampt_damage(amount)
+		
+func _attampt_damage(damage: float) -> void:
+	var health: float = tower_healt - damage
+	print("Tower health:", health)
+	if _tower_health(health):
+		print("Tower is destroyed")
+		player_tower.queue_free()
+	
+func _tower_health(health: float):
+	if health <= 0: return true
+	return false
