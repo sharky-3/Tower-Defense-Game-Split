e5cc19f242b1a962d4e2795450eb819ec3b2ab7f
sharky-3 <whitebunnypue@gmail.com>
2026-03-02 16:38:00 +0100

    Placing Position

--- Common/UI/Card/Card.gd
+++ Common/UI/Card/Card.gd
@@ -26,11 +26,6 @@ var tween_rot: Tween
 	// FUNCTIONS
 ]] """
 
-""" [[ ============================================================ ]] """
-""" [[ Ready ]] """
-func _ready() -> void:
-	pass
-
 """ [[ Process ]] """
 func _process(delta: float) -> void:
 	if state == 1:
@@ -41,11 +36,62 @@ func _process(delta: float) -> void:
 		rotation = 0
 		if Input.is_action_just_released("left_click"): 
 			Global.IS_DRAGGING_CARD = false
+			
+			set_drag_visuals(false)
+			spawn_cube_at_mouse()
+			
 			state = 0
 			position = in_hand_pos
 			rotation = in_hand_rot
-
+			
 """ [[ ============================================================ ]] """
+""" [[ Place ]] """
+func spawn_cube_at_mouse():
+
+	var camera: Camera3D = get_viewport().get_camera_3d()
+	if camera == null:
+		print("No camera")
+		return
+
+	var mouse_pos: Vector2 = get_viewport().get_mouse_position()
+
+	var ray_origin: Vector3 = camera.project_ray_origin(mouse_pos)
+	var ray_dir: Vector3 = camera.project_ray_normal(mouse_pos)
+	var ray_end: Vector3 = ray_origin + ray_dir * 1000.0
+
+	print("origin: ", ray_origin)
+	print("end: ", ray_end)
+
+	var space_state = camera.get_world_3d().direct_space_state
+
+	var query = PhysicsRayQueryParameters3D.new()
+	query.from = ray_origin
+	query.to = ray_end
+	query.collide_with_areas = true
+	query.collide_with_bodies = true
+
+	var result = space_state.intersect_ray(query)
+
+	print("result: ", result)
+
+	if result.has("position"):
+		var cube = MeshInstance3D.new()
+		cube.mesh = BoxMesh.new()
+		get_tree().current_scene.add_child(cube)
+		cube.global_position = result.position
+		
+""" [[ Drag ]] """
+func set_drag_visuals(is_dragging: bool) -> void:
+	print(is_dragging)
+	var tween = create_tween().set_parallel(true)
+
+	if is_dragging:
+		tween.tween_property(self, "scale", Vector2(0.8, 0.8), 0.15)
+		tween.tween_property(self, "modulate:a", 0.7, 0.15)
+	else:
+		tween.tween_property(self, "scale", Vector2.ONE, 0.2)
+		tween.tween_property(self, "modulate:a", 1.0, 0.2)
+
 """ [[ Card Interaction ]] """
 func _on_gui_input(event: InputEvent) -> void:
 	if state == 1: return
@@ -53,6 +99,8 @@ func _on_gui_input(event: InputEvent) -> void:
 		state = 1
 		offset = get_global_mouse_position() - position
 		z_index = 10
+		
+		set_drag_visuals(true)
 	
 	if not event is InputEventMouseMotion: return
 	
@@ -120,3 +168,8 @@ func tween_anim(type: int):
 				self, "scale", Vector2.ONE, 0.55
 			).set_trans(Tween.TRANS_ELASTIC).set_ease(Tween.EASE_OUT)
 	await tween.finished
+
+""" [[ ============================================================ ]] """
+""" [[ Interaction ]] """
+func on_card_clicked(name: String) -> void:
+	print("Clicked: ", name)
