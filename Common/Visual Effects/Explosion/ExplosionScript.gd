eb44a5b17eaec7ee60d9d8b8bee9052ee5f8a818
sharky-3 <whitebunnypue@gmail.com>
2026-03-02 08:44:11 +0100

    vfx fix

--- Common/Visual Effects/Explosion/ExplosionScript.gd
+++ Common/Visual Effects/Explosion/ExplosionScript.gd
@@ -1,14 +1,22 @@
 extends Node3D
 
-@onready var debris: GPUParticles3D = $Debris
-@onready var smoke: GPUParticles3D = $Smoke
-@onready var fire: GPUParticles3D = $Fire
-@onready var explosion_sound: AudioStreamPlayer3D = $explosionSound
+@onready var debris := get_node_or_null("Debris")
+@onready var smoke := get_node_or_null("Smoke")
+@onready var fire := get_node_or_null("Fire")
+@onready var explosion_sound := get_node_or_null("explosionSound")
+
+var timer := 0.0
+var interval := 3.0   # time between explosions
+
+func _process(delta: float) -> void:
+	timer += delta
+	
+	if timer >= interval:
+		timer = 0.0
+		explode()
 
 func explode():
-	debris.emitting = true
-	smoke.emitting = true
-	fire.emitting = true
-	explosion_sound.play()
-	await get_tree().create_timer(2.0).timeout
-	queue_free()
+	if debris: debris.restart()
+	if smoke: smoke.restart()
+	if fire: fire.restart()
+	if explosion_sound: explosion_sound.play()
