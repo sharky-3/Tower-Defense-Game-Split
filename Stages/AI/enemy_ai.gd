d57e7525b14d52ba38bbc86f6bc6b7072b6fd15d
sharky-3 <whitebunnypue@gmail.com>
2026-03-04 13:08:05 +0100

    AI

--- Stages/AI/enemy_ai.gd
+++ Stages/AI/enemy_ai.gd
@@ -2,13 +2,28 @@
 extends Node3D
 """ [[ ============================================================ ]] """
 
+""" [[ Constants / Exported Data ]] """
+@export var speed: float = 1.0
+@export var health: float = 100.0
+@export var damage: float = 6.0
+@export var size: float = 1.0
+
+@export var rewards: Dictionary = {
+	"Gold": 5,
+	"Exp": 1,
+}
+
+@export var deathSound: AudioStream
+
 """ [[ Node references ]] """
+@onready var character: MeshInstance3D = $Mesh
+@onready var rigid_body: RigidBody3D = $Mesh/RigidBody3D
 @onready var rayCastGroup: Node = $Ray
-@onready var player_tower: Node3D = $"../Map/PlayerTower"
+
+@onready var target: Node3D = $"../Map/PlayerTower"
 
 """ [[ Stats ]] """
 var rays := {}
-var speed: float = 2.0
 var turn_speed: float = 10
 
 """ [[ ============================================================ ]] """
@@ -32,14 +47,31 @@ func _process(delta: float) -> void:
 	translate(Vector3.FORWARD * current_speed * delta)
 
 """ [[ ============================================================ ]] """
+""" [[ Initialize ]] """
+func set_character(mesh: Mesh):
+	self.character.mesh = mesh
+	self.character.scale = Vector3( size, size, size )
+
+func set_stats(dictionary: Dictionary):
+	self.speed = dictionary.get("speed", 1.0)
+	self.health = dictionary.get("health", 1.0)
+	self.damage = dictionary.get("damage", 1.0)
+	self.size = dictionary.get("scale", 1.0)
+	
+	set_character(self.character.mesh)
+
+func set_rewards(dictionary: Dictionary):
+	self.rewards["Gold"] = dictionary.get("Gold", 0.0)
+	self.rewards["Exp"] = dictionary.get("Exp", 0.0)
+
+func set_group(): self.rigid_body.add_to_group("Enemy")
+
+""" [[ ============================================================ ]] """
 """ [[ Functions ]] """
 
-func move_forward(delta: float) -> void:
-	translate(Vector3.FORWARD * speed * delta)
-
 func look_at_tower(delta: float) -> bool:
-	if not player_tower: return false
-	var dir_to_tower = player_tower.global_position - global_position
+	if not target: return false
+	var dir_to_tower = target.global_position - global_position
 	dir_to_tower.y = 0
 	
 	if dir_to_tower.length_squared() < 0.001: return false
@@ -47,6 +79,9 @@ func look_at_tower(delta: float) -> bool:
 	rotation.y = lerp_angle(rotation.y, target_angle, 3.0 * delta)
 	
 	return true
+
+func move_forward(delta: float) -> void:
+	translate(Vector3.FORWARD * speed * delta)
 	
 func get_avoidance_steer() -> float:
 	var steer := 0.0
@@ -58,3 +93,26 @@ func get_avoidance_steer() -> float:
 	elif left: steer = -1.0
 	
 	return steer
+
+func take_damage(value: float):
+	self.health -= value
+	if self.health <= 0: character_die()
+	_global_update_player_stats("Total_Damage_Dealted", value)
+
+
+func character_die():
+	_global_update_player_stats("Enemies_Killed", 1)
+	_global_update_player_stats("Gold", self.rewards["Gold"])
+	_global_update_player_stats("Exp", self.rewards["Exp"])
+	queue_free()
+
+func reached_target(): 
+	if target and target.has_method("take_attack_damage"):
+		target.take_attack_damage(self.damage)
+	character_die()
+	
+""" [[ ============================================================ ]] """
+""" [[ Globals ]] """
+
+func _global_update_player_stats(_name: String, value: float):
+	Global.update_player_game_stats(_name, value)
